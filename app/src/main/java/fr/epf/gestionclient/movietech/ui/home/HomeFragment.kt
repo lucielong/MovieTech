aa7475c775989f282c1c155adc71c91cf52def62
lucielong <lucie.long@epfedu.fr>
2023-05-25 11:31:39 +0200

    :sparkles: features : movie view

--- app/src/main/java/fr/epf/gestionclient/movietech/ui/home/HomeFragment.kt
+++ app/src/main/java/fr/epf/gestionclient/movietech/ui/home/HomeFragment.kt
@@ -1,9 +1,12 @@
 package fr.epf.gestionclient.movietech.ui.home
 
+import android.R
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -24,7 +27,6 @@ class HomeFragment : Fragment() {
     ): View {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         homeViewModel.text.observe(viewLifecycleOwner) {
--- app/src/main/java/fr/epf/gestionclient/movietech/ui/home/HomeViewModel.kt
+++ app/src/main/java/fr/epf/gestionclient/movietech/ui/home/HomeViewModel.kt
@@ -1,13 +1,20 @@
 package fr.epf.gestionclient.movietech.ui.home
 
+import android.R
+import android.view.Gravity
+import android.widget.SearchView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
 class HomeViewModel : ViewModel() {
+
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
+
     }
+
     val text: LiveData<String> = _text
 }
