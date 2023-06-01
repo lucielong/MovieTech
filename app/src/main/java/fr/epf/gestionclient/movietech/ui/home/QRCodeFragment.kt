ba01903d1c0c1c1c750742a0d5016371ec23eb40
lucielong <lucie.long@epfedu.fr>
2023-06-01 13:45:53 +0200

    ✨ features : QR code turn into a fragment and can lead to the details page

--- app/src/main/java/fr/epf/gestionclient/movietech/ui/home/QRCodeFragment.kt
+++ app/src/main/java/fr/epf/gestionclient/movietech/ui/home/QRCodeFragment.kt
@@ -1,6 +1,5 @@
 package fr.epf.gestionclient.movietech.ui.home
 
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
@@ -9,62 +8,69 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
-import fr.epf.gestionclient.movietech.MovieDetails
 import fr.epf.gestionclient.movietech.R
-import fr.epf.gestionclient.movietech.databinding.ActivityQrcodeBinding
-import fr.epf.gestionclient.movietech.ui.details.MovieDetailsFragment
+import fr.epf.gestionclient.movietech.databinding.FragmentQrcodeBinding
 import java.io.File
 
-class QRCodeActivity : AppCompatActivity() {
+class QRCodeFragment : Fragment() {
 
-
-    private lateinit var binding: ActivityQrcodeBinding
+    private lateinit var binding: FragmentQrcodeBinding
     private var imageCapture: ImageCapture? = null
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentQrcodeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityQrcodeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         if (allPermissionGranted()) {
             startCamera()
         } else {
-            ActivityCompat.requestPermissions(
-                this, Constants.REQUIRED_PERMISSIONS, Constants.REQUEST_CODE_PERMISSIONS)
+            requestPermissions(
+                Constants.REQUIRED_PERMISSIONS,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
         }
-        binding.button.setOnClickListener {
-            takePhoto()
-        }
+
+        binding.button.setOnClickListener { takePhoto() }
+
     }
 
     private fun takePhoto() {
         val imageCapture = imageCapture ?: return
         val photoFile = File(
-            externalMediaDirs.firstOrNull(),
-            "${System.currentTimeMillis()}.jpg")
+            requireContext().externalMediaDirs.firstOrNull(),
+            "${System.currentTimeMillis()}.jpg"
+        )
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-        imageCapture.takePicture(
-            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback {
                 override fun onError(exc: ImageCaptureException) {
                     Log.e(Constants.TAG, "Photo capture failed: ${exc.message}", exc)
                 }
+
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
                     val savedUri = Uri.fromFile(photoFile)
-                    val image = InputImage.fromFilePath(this@QRCodeActivity, savedUri)
+                    val image = InputImage.fromFilePath(requireContext(), savedUri)
                     val scanner = BarcodeScanning.getClient()
                     scanner.process(image)
                         .addOnSuccessListener { barcodes ->
@@ -73,17 +79,14 @@ class QRCodeActivity : AppCompatActivity() {
                                 val corners = barcode.cornerPoints
                                 val rawValue = barcode.rawValue
                                 val valueType = barcode.valueType
-                                // See API reference for complete list of supported types
+
                                 when (valueType) {
                                     Barcode.TYPE_TEXT -> {
                                         val intValue = barcode.rawValue?.toIntOrNull()
                                         if (intValue != null) {
                                             Log.d(Constants.TAG, "onSuccess: $intValue")
                                             val bundle = bundleOf("movieId" to intValue)
-                                            val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigation_details)
-                                            Log.d(Constants.TAG, "navHostFragment: $navHostFragment")
-                                            val navController = navHostFragment?.findNavController()
-                                            Log.d(Constants.TAG, "navController: $navController")
+                                            findNavController().navigate(R.id.action_get_details_from_qrcode, bundle)
                                         }
                                     }
                                     Barcode.TYPE_URL -> {
@@ -100,27 +103,23 @@ class QRCodeActivity : AppCompatActivity() {
             })
     }
 
-    private fun startCamera(){
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
         cameraProviderFuture.addListener(Runnable {
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-            val preview = Preview.Builder()
-                .build()
-                .also {mPreview ->
+            val preview = Preview.Builder().build().also { mPreview ->
                 mPreview.setSurfaceProvider(binding.viewFinder.surfaceProvider)
             }
             imageCapture = ImageCapture.Builder().build()
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             try {
                 cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture)
-            } catch(e: Exception) {
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch (e: Exception) {
                 Log.d(Constants.TAG, "startCamera failed", e)
             }
-        }, ContextCompat.getMainExecutor(this))
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
-
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -128,20 +127,18 @@ class QRCodeActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
-            if(allPermissionGranted()) {
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
                 startCamera()
             } else {
-                Toast.makeText(this, "We Don't Have Permission", Toast.LENGTH_SHORT).show()
-                finish()
+                Toast.makeText(requireContext(), "We Don't Have Permission", Toast.LENGTH_SHORT).show()
+                activity?.finish()
             }
         }
     }
 
-
-
     private fun allPermissionGranted() = Constants.REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it) == PackageManager.PERMISSION_GRANTED
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
     }
 }
+
